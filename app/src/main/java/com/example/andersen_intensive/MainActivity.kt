6410282fae7589ec1ce44d03976818f4b762c0b4
Daria Filinkova <daria.filinkova@gmail.com>
2023-11-27 14:54:03 +0300

    added homework_1_flags

--- app/src/main/java/com/example/andersen_intensive/MainActivity.kt
+++ app/src/main/java/com/example/andersen_intensive/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.andersen_intensive
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.andersen_intensive.ui.components.ArrowDropDownUp
+import com.example.andersen_intensive.ui.components.Flags
 import com.example.andersen_intensive.ui.theme.AndersenintensiveTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,31 +31,45 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndersenintensiveTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Homework1()
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Homework1() {
+    val isClickedFlags = remember {
+        mutableStateOf(false)
+    }
+    Column() {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .border(
+                    1.dp, Color.Black,
+                    RectangleShape
+                ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Flags", modifier = Modifier.padding(16.dp))
+            Spacer(modifier = Modifier.weight(1f))
+            ArrowDropDownUp(isClicked = isClickedFlags)
+        }
+        if (isClickedFlags.value) {
+            Flags()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AndersenintensiveTheme {
-        Greeting("Android")
-    }
+    Flags()
 }
